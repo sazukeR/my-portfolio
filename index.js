831aa7318de0f22fd0716f9ce12fbf872e0e7ce6
sazukeR <reinaldojcg1@gmail.com>
2023-04-24 14:34:53 -0500

    Add work insertions functionality (vanilla js)

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import hamburguerMenu from './burger_menu.js';
 import showAndLessInfoAboutMe from './show_and_less_aboudMe.js';
+import worksInsertions from './works_insertions.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   hamburguerMenu('.menu-btn', '.panel', '.front-page');
   showAndLessInfoAboutMe('.info-btn', '.more-info');
+  worksInsertions();
 });
