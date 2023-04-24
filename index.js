ad26a2569f61d022a76485ba1ffe0b34b2901469
sazukeR <reinaldojcg1@gmail.com>
2023-04-24 12:08:52 -0500

    Add button show more functionality (vanilla js)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import hamburguerMenu from './burger_menu.js';
+import showAndLessInfoAboutMe from './show_and_less_aboudMe.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   hamburguerMenu('.menu-btn', '.panel', '.front-page');
+  showAndLessInfoAboutMe('.info-btn', '.more-info');
 });
